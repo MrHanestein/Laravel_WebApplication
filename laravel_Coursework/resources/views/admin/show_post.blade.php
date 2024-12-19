84dfe72994c00e16c3e990b055ff0f13badb84b7
MrHanestein <123433942+MrHanestein@users.noreply.github.com>
2024-12-19 13:48:39 -0800

    Implemented sweet alert delete notification.

--- laravel_Coursework/resources/views/admin/show_post.blade.php
+++ laravel_Coursework/resources/views/admin/show_post.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @include('admin.css')
     <style type="text/css">
         .title_deg
         {
-            font-size: 30px;
+            font-size: 20px;
             font-weight: bold;
             color: white;
-            padding: 30px;
+            padding: 20px;
             text-align: center;
         }
         .table_deg
         {
             border: 2px solid pink;
-            width: 85%;
+            width: 70%;
             text-align: center;
-            margin-left: 75px;
+            margin-left: 70px;
         }
         .th_deg
         {
@@ -24,7 +25,7 @@
         }
         .img_deg
         {
-            height: 110px;
+            height: 90px;
             width: 160px;
             padding: 10px;
         }
@@ -35,6 +36,12 @@
 <div class="d-flex align-items-stretch">
 @include('admin.sidebar')
     <div class="page-content">
+        @if(session()->has('message'))
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                {{session()->get('message')}}
+            </div>
+        @endif
         <h1 class="title_deg">All Post</h1>
         <table class="table_deg">
             <tr class="th_deg ">
@@ -44,6 +51,7 @@
                 <th>Post Status</th>
                 <th>UserType</th>
                 <th>Image</th>
+                <th>Delete</th>
             </tr>
             @foreach($post as $post)
             <tr>
@@ -55,11 +63,42 @@
                 <td>
                     <img class="img_deg" src="postimage/{{$post->image}}">
                 </td>
+                <td>
+                    <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger" onclick="confirmation(event)">Delete</a>
+                </td>
             </tr>
             @endforeach
         </table>
     </div>
 @include('admin.footer')
+    <script>
+        function confirmation(ev) {
+            ev.preventDefault();
+            var urlToRedirect = ev.currentTarget.getAttribute('href');
+            console.log(urlToRedirect);
+            swal({
+                title: "Are you sure to Delete this post",
+                text: "You will not be able to revert this!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willCancel) => {
+                    if (willCancel) {
+
+
+
+                        window.location.href = urlToRedirect;
+
+                    }
+
+
+                });
+
+
+        }
+    </script>
+</div>"
 </body>
 </html>
 
--- laravel_Coursework/routes/web.php
+++ laravel_Coursework/routes/web.php
@@ -16,3 +16,5 @@ Route::post('/add_post',[AdminController::class,'add_post']);
 Route::get('/post_page',[AdminController::class,'post_page']);
 
 Route::get('/show_post',[AdminController::class,'show_post']);
+
+Route::get('/delete_post/{id}',[AdminController::class,'delete_post']);
