ca0e1521f34c0c4c296533746086f4a41e9dfe28
MrHanestein <123433942+MrHanestein@users.noreply.github.com>
2024-12-19 11:32:27 -0800

    Updated Admin dashboard to add data in post table on php database

--- laravel_Coursework/routes/web.php
+++ laravel_Coursework/routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/',[HomeController::class,'homepage']);
 
 
 
 route::get('/home',[HomeController::class,'index'])->name('home');
+
+Route::get('/post_page',[AdminController::class,'post_page']);
+
+Route::post('/add_post',[AdminController::class,'add_post']);
